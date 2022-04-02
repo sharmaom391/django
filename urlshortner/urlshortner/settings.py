b390dc05b4f6311755cd9c868b2d74281ef3133d
om sharma <sharmaom391@gmail.com>
2022-04-02 18:30:46 +0530

    Om:deliberate commit

--- urlshortner/urlshortner/settings.py
+++ urlshortner/urlshortner/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$zjktpjehris&hz^yq)agekaoke!xzg3qvcmw0fimo)kbclytl
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-
+ 
 ALLOWED_HOSTS = ['*']
 
 
